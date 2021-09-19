6a57ac228930f304c81fda2e7d0ba97d391db165
Thiago L <thiago.lopes.dev@gmail.com>
2021-09-19 01:03:58 -0300

    chore: add service

--- src/main.tf
+++ src/main.tf
@@ -1,1 +1,13 @@
 # Only for testing
+
+provider "azurerm" {
+    version = "~>1.32.0"
+    use_msi = true
+    subscription_id = "4763878c-4e3b-4099-98f3-f58a64b3a9bd"
+    tenant_id = "590c9855-543c-47d3-b0ae-9b3df2ae76c0"
+}
+
+resource "azurerm_resource_group" "rg" {
+    name = "rg-terraform"
+    location = "eastus2"
+}
